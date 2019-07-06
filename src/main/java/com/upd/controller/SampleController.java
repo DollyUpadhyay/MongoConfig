c4e262be754557226072d7375afbc06a1e1218fb
Dolly <dolly.upadhyay@gingerwebs.co.in>
2019-07-06 19:18:00 +0530

    changes in files for configuration

--- src/main/java/com/upd/controller/SampleController.java
+++ src/main/java/com/upd/controller/SampleController.java
@@ -1,8 +1,27 @@
 package com.upd.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.upd.service.SampleService;
+
+@RestController
 public class SampleController {
 
-	
+	/**
+	 * autowired 
+	 */
+	@Autowired
+	SampleService sample_service;
+	@RequestMapping(value="sample",method=RequestMethod.POST)
+	public String sample() {
+		/**
+		 * to insert
+		 */
+		return sample_service.insert();
+		
+	}
 	
 }
